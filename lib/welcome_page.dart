5af3a2e29e9fc383df748b8a0d10b894e89f6842
samihaa11 <samihaa107@gmail.com>
2026-09-09 00:56:21 +0600

    new welcome screen with login and signup navigator

--- lib/welcome_page.dart
+++ lib/welcome_page.dart
@@ -8,98 +8,106 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-          body: SafeArea(
-            child: Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height,
-              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: MediaQuery
+              .of(context)
+              .size
+              .height,
+          padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Welcome to Ecomart!",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text("Shop sustainably, save earth",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(
+                height: 90,
+              ),
+              Container(
+                height: MediaQuery
+                    .of(context)
+                    .size
+                    .height / 3,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/welcome.png")
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 60,
+              ),
+              Column(
                 children: <Widget>[
-                  Text(
-                    "Welcome to Ecomart!",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30,
-
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Text("Shop sustainably, save earth",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.grey[700],
-                    fontSize: 16,
-                  ),
-                  ),
-                  SizedBox(
-                    height: 90,
-                  ),
-                  Container(
-                    height: MediaQuery.of(context).size.height / 3,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage("assets/images/welcome.png")
-                      ),
-                    ),
-                  ),
-                  SizedBox(
+                  MaterialButton(
+                    minWidth: double.infinity,
                     height: 60,
-                  ),
-                  Column(
-                    children: <Widget>[
-                      MaterialButton(
-                        minWidth: double.infinity,
-                          height: 60,
-                          onPressed: (){
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()));
-                          },
-                        shape: RoundedRectangleBorder(
-                          side: BorderSide(
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => LoginPage()));
+                    },
+                    shape: RoundedRectangleBorder(
+                        side: BorderSide(
                             color: Colors.black
-                          ),
-                          borderRadius: BorderRadius.circular(50)
                         ),
-                        child: Text(
-                          "Login",
-                           style: TextStyle(
-                             fontWeight: FontWeight.w600,
-                             fontSize: 18
-                           ),
-                        ),
+                        borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: Text(
+                      "Login",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18
                       ),
+                    ),
+                  ),
 
-                      SizedBox(
-                        height: 10,
+                  SizedBox(
+                    height: 10,
+                  ),
+                  MaterialButton(
+                    minWidth: double.infinity,
+                    height: 60,
+                    onPressed: () {
+                      Navigator.push(context, MaterialPageRoute(
+                          builder: (context) => SignupPage()));
+                    },
+                    color: Colors.green[800],
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50)
+                    ),
+                    child: Text(
+                      "Sign-up",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18
                       ),
-                      MaterialButton(
-                        minWidth: double.infinity,
-                        height: 60,
-                        onPressed: (){
-                          Navigator.push(context, MaterialPageRoute(builder: (context)=>SignupPage()));
-                        },
-                        color: Colors.green[800],
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50)
-                        ),
-                        child: Text(
-                          "Sign-up",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 18
-                          ),
-                        ),
-                      )
-                    ],
+                    ),
                   )
                 ],
-              ),
+              )
+            ],
           ),
-       ),
+        ),
+      ),
     );
   }
 }
