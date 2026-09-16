3cf75446f3a7a423a6483ca188810bc8c93f1559
XxxeddD <gamerroblox00007@gmail.com>
2026-09-16 23:09:38 +0600

    added product fields or variables

---
+++ lib/product.dart
@@ -0,0 +1,19 @@
+class Product{
+  final int id;
+  final String name;
+  final String image;
+  final String category;
+  final String description;
+  final double price;
+  int quantity;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.image,
+    required this.category,
+    required this.description,
+    required this.price,
+    required this.quantity
+  });
+}
