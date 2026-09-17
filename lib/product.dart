99f40db5a484f80038461e783dbd3fc81976b93f
XxxeddD <gamerroblox00007@gmail.com>
2026-09-17 22:38:45 +0600

    added new files

--- lib/product.dart
+++ lib/product.dart
@@ -17,3 +17,26 @@ class Product{
     required this.quantity
   });
 }
+
+List<Product> products = [
+  Product(
+    id: 2056,
+    name: 'Bamboo Toothbrush',
+    image: 'assets/images/bamboo_toothbrush.png',
+    category: 'Personal Care',
+    description: 'Eco-friendly toothbrush made from sustainable bamboo.',
+    price: 3.99,
+    quantity: 25,
+  ),
+
+  Product(
+    id: 3327,
+    name: 'Paper Pen',
+    image: 'assets/images/bamboo_toothbrush.png',
+    category: 'Writing',
+    description: 'Black ink pen made from hard paper, easy and comfortable to write with.',
+    price: 0.99,
+    quantity: 47,
+  ),
+
+];
