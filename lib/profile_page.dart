d2fd3cf7f1b2355864d3be97e3a263260cfdf5c4
samihaa11 <samihaa107@gmail.com>
2026-09-18 18:33:28 +0600

    profile page UI

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,14 +1,64 @@
+import 'package:ecomart/text_box.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
-
   @override
   State<ProfilePage> createState() => _ProfilePageState();
 }
-
 class _ProfilePageState extends State<ProfilePage> {
+  final currentUser = FirebaseAuth.instance.currentUser!;
+  Future<void> editField(String field) async{
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.green[50],
+      appBar: AppBar(
+        title: Text(
+          'Profile Page',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: ListView(
+        children: [
+          const SizedBox(height: 50),
+          Icon(
+            Icons.person,
+            size: 72,
+          ),
+
+          const SizedBox(height: 10),
+
+          Text(
+              currentUser.email!,
+              textAlign: TextAlign.center,
+          ),
+
+          const SizedBox(height: 50),
+
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0),
+            child: Text('My details'),
+          ),
+          MyTextBox(
+              text: 'koko',
+              sectionName: 'username',
+              onPressed: () => editField('username'),
+          ),
+          MyTextBox(
+            text: '',
+            sectionName: 'Address',
+            onPressed: () => editField('Address'),
+          ),
+        ],
+
+      ),
+    );
   }
 }
